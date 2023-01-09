d2f814a0ed5ad8c26b70869249e3b86433904ee4
rldettloff <114546927+rldettloff@users.noreply.github.com>
2023-01-09 02:46:28 +0000

    Committing week 1 file

---
+++ week1/script.js
@@ -0,0 +1,10 @@
+let firstName = "Ryder";
+
+let lastName = "Dettloff";
+
+let yearsOfStudy = "1 Quarter";
+
+let goal = "Become a Software Developer"
+
+document.write("My name is" + firstName + " " + lastName + ". I have been at SCC for" + yearsOfStudy + "and plan to" + goal + "after completing my courses.");
+
