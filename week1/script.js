44004f5d0c85dd162cb0fb9e5d5ce4f220325763
rldettloff <114546927+rldettloff@users.noreply.github.com>
2023-01-10 02:10:20 +0000

    Updated html to contain output!

--- week1/script.js
+++ week1/script.js
@@ -5,6 +5,3 @@ let lastName = "Dettloff";
 let yearsOfStudy = "1 Quarter";
 
 let goal = "Become a Software Developer"
-
-document.write("My name is" + firstName + " " + lastName + ". I have been at SCC for" + yearsOfStudy + "and plan to" + goal + "after completing my courses.");
-
